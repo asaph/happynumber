a135009577fb0e74e56e416497f80f1400c463a5
asaph <ace@dontletgo.com>
2015-05-03 18:32:27 -0700

    Add a class the prints all the happy numbers between 1 and 1000 inclusive

---
+++ src/org/asaph/happynumber/HappyNumber.java
@@ -0,0 +1,42 @@
+package org.asaph.happynumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HappyNumber {
+	public static int nextNumber(int number) {
+		String numberString = Integer.toString(number);
+		int sum=0;
+		for (int i=0, length=numberString.length(); i<length; i++) {
+			char ch = numberString.charAt(i);
+			int digit = Integer.parseInt("" + ch);
+			int square = digit * digit;
+			sum += square;
+		}
+		return sum;
+	}
+
+	public static boolean isHappy(int number) {
+		return isHappy(number, new ArrayList<Integer>());
+	}
+
+	public static boolean isHappy(int number, List<Integer> numbersSoFar) {
+		int next = nextNumber(number);
+		if (next == 1) {
+			return true;
+		}
+		if (numbersSoFar.contains(next)) {
+			return false;
+		}
+		numbersSoFar.add(next);
+		return isHappy(next, numbersSoFar);
+	}
+
+	public static void main(String[] args) {
+		for (int i=1; i<=1000; i++) {
+			if (isHappy(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
